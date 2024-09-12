b0a6e217a0bd1fd67b3af7cd97709972cc42515a
Vaibhav Gupta <vaibhavgupta1071997@gmail.com>
2024-09-12 20:48:11 +0530

    firebase config

--- src/config/firebaseAuth.js
+++ src/config/firebaseAuth.js
@@ -2,7 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { GoogleAuthProvider } from "firebase/auth/web-extension";
 
-const firebaseConfig = JSON.parse(import.meta.env.VITE_KEY);
+const firebaseConfig = {
+  "apiKey": import.meta.env.VITE_APIKEY,
+  "authDomain":import.meta.env.VITE_AUTHDOMAIN,
+  "projectId": import.meta.env.VITE_PROJECTID,
+  "storageBucket":import.meta.env.VITE_STORAGEBUCKET,
+  "messagingSenderId": import.meta.env.VITE_MESSAGINGSENDERID,
+  "appId":import.meta.env.VITE_APPID
+}
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
